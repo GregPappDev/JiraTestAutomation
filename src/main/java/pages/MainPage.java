2b0cb72fcc3ff5648b28a8efa73dbe2f024822bb
GregPappDev <gregpappdev@gmail.com>
2023-11-22 18:24:50 +0100

    correct property types of MainPage

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class MainPage {
-    final private String avatarId = "aui-avatar-inner";
+    final private String avatarClassName = "aui-avatar-inner";
     final private String logoutOptionId = "log_out";
 
 
     public WebElement getAvatarId(WebDriver driver) {
-        return driver.findElement(By.id(avatarId));
+        return driver.findElement(By.className(avatarClassName));
     }
 
     public WebElement getLogoutOptionId(WebDriver driver) {
